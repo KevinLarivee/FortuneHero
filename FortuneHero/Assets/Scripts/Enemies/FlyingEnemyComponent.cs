a5b29ed6c027efd4cf8863c2a55eb7d0784952dc
KevinLarivee <kevinlarivee360@gmail.com>
2025-09-29 21:26:05 -0400

    flyingComponent

--- FortuneHero/Assets/Scripts/Enemies/FlyingEnemyComponent.cs
+++ FortuneHero/Assets/Scripts/Enemies/FlyingEnemyComponent.cs
@@ -1,67 +1,96 @@
 using System.Net;
+using Unity.VisualScripting;
 using UnityEngine;
 
+[RequireComponent(typeof(PatrolComponent), typeof(DetectorComponent))]
 public class FlyingEnemyComponent : MonoBehaviour
 {
     Animator animator;
+    PatrolComponent patrol;
+    DetectorComponent detector;
 
     [SerializeField] GameObject player;
     [SerializeField] float moveSpeed = 5f;
-    [SerializeField] float stopDistance = 3f;
+    [SerializeField] float attackStopDistance = 3f;
+    [SerializeField] float patrolStopDistance = 0.5f;
+
 
     float timeUntilPatrol = 10f;
     float timeUntilPatrolTimer = 0f;
     float rotationSpeed = 5f;
-    
-    bool canAttack = false;
+
     [SerializeField] bool isDetecting = false; //Placeholder
 
     Vector3 target;
 
+    enum EnemyState { Patrol, Attacking, Chasing }
+    EnemyState enemyState;
 
     void Start()
     {
         animator = GetComponentInParent<Animator>();
+        patrol = GetComponent<PatrolComponent>();
+        patrol.move = Move;
+        detector = GetComponent<DetectorComponent>();
+        detector.targetDetected = PlayerDetected;
     }
 
     void Update()
     {
-        if (isDetecting)
-        {
-            PlayerDetected();
-            if(timeUntilPatrolTimer > 0) //si il detecte de nouveau apres avoir parti le timer = reset
-                timeUntilPatrolTimer = 0;
-        }
-        else //Detecte pas le joueur
-        {
+        if (enemyState == EnemyState.Chasing /*&& voit pas l'ennemi*/)
             timeUntilPatrolTimer += Time.deltaTime; //start le timer 
-            if (timeUntilPatrolTimer >= timeUntilPatrol) //si le timer atteint le max:
-            {
-                //Enable le patrol
-                animator.SetBool("isPatrolling", true);
-            }
-        } 
+
+        if (timeUntilPatrolTimer >= timeUntilPatrol) //si le timer atteint le max:
+        {
+            //Enable le patrol
+            patrol.isActive = true;
+            enemyState = EnemyState.Patrol;
+        }
+
     }
 
-    public void PlayerDetected()
+    public void PlayerDetected(Vector3 targetPosition)
     {
-        animator.SetBool("isPatrolling", false);
-        target = player.transform.position; //La target est la Pos du joueur recu quand l'enemy detecte le joueur
-        Vector3 posToTarget = target - transform.parent.position;
-        canAttack = posToTarget.sqrMagnitude <= stopDistance * stopDistance;
+        timeUntilPatrolTimer = 0;
+        patrol.isActive = false;
+        enemyState = EnemyState.Chasing;
 
-        if (canAttack)
+        target = targetPosition;
+        Vector3 posToTarget = target - transform.parent.position;
+
+
+        //    case EnemyState.Chasing:
+        //    transform.parent.position = Vector3.MoveTowards(transform.parent.position, target, moveSpeed * Time.deltaTime);
+        //    Quaternion targetRotation = Quaternion.LookRotation(posToTarget);
+        //    transform.parent.rotation = Quaternion.Slerp(transform.parent.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+        //    if (posToTarget.sqrMagnitude <= attackStopDistance * attackStopDistance)
+        //        enemyState = EnemyState.Attacking;
+
+        //    animator.SetBool("isPatrolling", false);
+        //    animator.SetBool("isChasing", true);
+        //    break;
+
+        //case EnemyState.Attacking:
+        //    Attack();
+        //    animator.SetBool("isChasing", false);
+        //    break;
+        //}
+
+    }
+
+    void Move(Transform newTarget)
+    {
+        target = newTarget.position;
+        Vector3 posToTarget = target - transform.parent.position;
+
+        if (posToTarget.sqrMagnitude <= patrolStopDistance * patrolStopDistance)
         {
-            Attack();
-            animator.SetBool("isChasing", false);
+            newTarget = patrol.NextTarget();
         }
-        else //Disable just le mouvement pour attaquer
-        {
-            transform.parent.position = Vector3.MoveTowards(transform.parent.position, target, moveSpeed * Time.deltaTime);
-            animator.SetBool("isChasing", true);
-        }
-        Quaternion targetRotation = Quaternion.LookRotation(posToTarget);
-        transform.parent.rotation = Quaternion.Slerp(transform.parent.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+        animator.SetBool("isPatrolling", true);
+        animator.SetBool("isChasing", false);
     }
 
     public void Attack()
