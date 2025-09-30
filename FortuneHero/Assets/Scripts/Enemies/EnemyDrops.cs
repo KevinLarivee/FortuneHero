26a9a1bd11c76bca7c53bee0f9f4b48fa2dbd75d
cric1 <109307071+cric1@users.noreply.github.com>
2025-09-30 11:44:28 -0400

    laptop
    laptop

--- FortuneHero/Assets/Scripts/Enemies/EnemyDrops.cs
+++ FortuneHero/Assets/Scripts/Enemies/EnemyDrops.cs
@@ -1,29 +1,29 @@
 using NUnit.Framework;
 using UnityEngine;
 using System.Collections.Generic;
+using System;
 
 public class EnemyDrops : MonoBehaviour
 {
-    [SerializeField] bool isdropped = false;
-    [SerializeField] GameObject XpOrb;
-    [SerializeField] GameObject CoinsOrb;
-
-    [Header("Paramètres de drop")]
-    [SerializeField] int XpCount = 3;
-    [SerializeField] int CoinsCount = 3;
-
+    [Serializable]
+    public class Drop
+    {
+        public GameObject ObjectToDrop;
+        public int dropSize = 10;
+    }
+    [SerializeField] List<Drop> drops;
+    
     [Header("Paramètres de position")]
     [SerializeField] float spread = 1.5f;
     [SerializeField] float heightOffset = 0.5f;
 
-    Dictionary<GameObject, int> drops = new Dictionary<GameObject, int>();
+  
 
 
 
     private void Start()
     {
-        drops.Add(XpOrb, XpCount);
-        drops.Add(CoinsOrb, CoinsCount);
+       
     }
 
     void Update()
@@ -60,13 +60,11 @@ public class EnemyDrops : MonoBehaviour
 
         foreach (var d in drops)
         {
-            if (d.Key == null || d.Value <= 0) continue;
-
-            for (int i = 0; i < d.Value; i++)
+            for (int i = 0; i < d.dropSize; i++)
             {
-                Vector2 rnd = Random.insideUnitCircle * spread;
+                Vector2 rnd = UnityEngine.Random.insideUnitCircle * spread;
                 Vector3 pos = new Vector3(basePos.x + rnd.x, basePos.y + heightOffset, basePos.z + rnd.y);
-                Instantiate(d.Key, pos, Quaternion.identity);
+                Instantiate(d.ObjectToDrop, pos, Quaternion.identity);
             }
         }
     }
