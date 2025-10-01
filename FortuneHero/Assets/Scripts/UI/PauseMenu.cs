5bdb0d8ebd87ba376f5d43d4440f98860bac1606
NoaBigS <noahbrassard1@gmail.com>
2025-10-01 12:34:27 -0400

    temp

--- FortuneHero/Assets/Scripts/UI/PauseMenu.cs
+++ FortuneHero/Assets/Scripts/UI/PauseMenu.cs
@@ -8,18 +8,12 @@ public class PauseMenu : MonoBehaviour
 {
     [SerializeField] private GameObject pauseMenuUI; 
     private bool isPaused = false;
-    [SerializeField] private Button boutonResumeGame;
-    [SerializeField] private Button boutonOptions;
-    [SerializeField] private Button boutonRetournerLobby;
     [SerializeField] private GameObject panelParametres;
 
     PlayerComponent player;
 
     private void Start()
     {
-        boutonResumeGame.onClick.AddListener(ResumeGame);
-        boutonOptions.onClick.AddListener(Options);
-        boutonRetournerLobby.onClick.AddListener(ReturnToLobby);
         panelParametres.GetComponent<MenuOption>().previous = Retour;
         player = PlayerComponent.Instance;
     }
@@ -55,7 +49,7 @@ public class PauseMenu : MonoBehaviour
     public void ReturnToLobby()
     {
         Time.timeScale = 1f;
-        LoadManager.Instance.Load("MainScene");
+        LoadManager.Instance.Load("Test");
     }
 
     public void Options()
