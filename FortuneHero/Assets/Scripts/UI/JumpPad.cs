a7c08201cd651b7fd113f30888cbfccd3e355e09
NoaBigS <noahbrassard1@gmail.com>
2025-10-15 13:53:12 -0400

    push

--- FortuneHero/Assets/Scripts/UI/JumpPad.cs
+++ FortuneHero/Assets/Scripts/UI/JumpPad.cs
@@ -2,22 +2,19 @@ using UnityEngine;
 
 public class JumpPad : MonoBehaviour
 {
-    Rigidbody rb;
     public float jumpForce = 20f;
-    private void Start()
-    {
-         rb = GetComponent<Rigidbody>();
-    }
+    
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && other.excludeLayers != LayerMask.GetMask("IgnoreTrigger")) 
         {
-            if (rb != null)
+            Debug.Log("jump");
+            PlayerMovement player = other.GetComponent<PlayerMovement>(); // ou le nom de ton script
+            if (player != null)
             {
-                //rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
-
-                rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+                //player.SetJumpPadForce(transform.up * jumpForce);
+               
             }
         }
     }
